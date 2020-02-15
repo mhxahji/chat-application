004dac7f57d5a5a5f9463467650b9824fc245f57
Mijail Gamarra <exmija16@gmail.com>
2020-02-15 19:54:03 -0300

    Adding the basics of django channels

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # chat-application
 simple browser-based chat application using Python.
+
+## Installation
+
+1. Clone this repository: `git clone https://github.com/mhxahji/chat-application.git`.
+2. Create a Virtual Environment and activate it
+3. `cd` into `chat-application`.
+4. Install all requirements with `pip install -r requirements.txt`.
+5. Migrate database with `python manage.py migrate`.
--- chatApplication/settings.py
+++ chatApplication/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chat',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Channels
+ASGI_APPLICATION = 'chatApplication.routing.application'
